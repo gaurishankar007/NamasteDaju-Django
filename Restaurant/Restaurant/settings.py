79aec9d1eaa68a9303d0ae6cb871d8bc2fa9fc5e
gaurishankar007 <gaurisharma358@gmail.com>
2021-03-16 19:15:55 +0545

    Updating database from sqlite to Mysql

--- Restaurant/Restaurant/settings.py
+++ Restaurant/Restaurant/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'Restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurant',
+        'USER': 'root',
+        'PASSWORD': 'Mysql@358',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
